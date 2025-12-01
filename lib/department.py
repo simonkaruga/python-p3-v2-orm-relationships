a6fc14fe5370016e5d547f36296f28a6ef7d5edb
simonkaruga <simonkaruganjoroge@gmail.com>
2025-12-01 08:26:15 +0300

    Add origin

--- lib/department.py
+++ lib/department.py
@@ -139,3 +139,16 @@ class Department:
 
         row = CURSOR.execute(sql, (name,)).fetchone()
         return cls.instance_from_db(row) if row else None
+
+    def employees(self):
+        """Return a list of Employee objects associated with this department"""
+        from employee import Employee  # avoid circular import
+
+        sql = """
+            SELECT *
+            FROM employees
+            WHERE department_id = ?
+        """
+        rows = CURSOR.execute(sql, (self.id,)).fetchall()
+
+        return [Employee.instance_from_db(row) for row in rows]
